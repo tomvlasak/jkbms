987bd8330b87fb546b1cec79784ebafd6dcc0d81
tomvlasak <tom.vlasak@gmail.com>
2024-09-11 12:24:05 +0200

    Update test.py

--- test.py
+++ test.py
@@ -350,7 +350,7 @@ def gather_and_send_data():
         bytes_written = s.write(request_FRAME)
         print(f"wrote {bytes_written} bytes")
 
-        full_response = s.read(512)
+        full_response = s.read(300)
         read_time = time.time() - read_start_time
         print(f"Full response: {full_response.hex()}")
         print(f"Response read took: {read_time:.4f} seconds")
